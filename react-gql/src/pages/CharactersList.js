7263c9e1c356a27d97a475b318cf782df8ba4b03
nickshakh <shakhbazyann@gmail.com>
2024-02-25 16:33:44 +0400

    feat- made a custom hook

--- react-gql/src/pages/CharactersList.js
+++ react-gql/src/pages/CharactersList.js
@@ -1,23 +1,12 @@
 import React from 'react'
-import { useQuery,gql } from '@apollo/client'
 import "./CharactersList.css"
+import { useCharacters } from '../hooks/useCharacters'
 
-const GET_CHARACTERS = gql`
-query {
-  characters{
-    results{
-      id
-      name
-      image
-      type
-    }
-  }
-}`
+
 
 export default function CharactersList() {
 
-    const {loading,error,data} = useQuery(GET_CHARACTERS)
-    console.log({loading,error,data})
+    const {error,loading,data} = useCharacters()
 
     if(loading) {return <h1>Loading...</h1>}
     
@@ -25,11 +14,12 @@ export default function CharactersList() {
 
   return (
     <div className='CharactersList'>
-        {data.characters.results.map(character => {
-            return <div>
-                <img src={character.image} alt={character.name + " image"}/>
+        {data.characters.results.map((character) => {
+            return(<div>
+                <img src={character.image} alt={character.name + "'s image"}/>
                 <h2>{character.name}</h2>
             </div>
+            );
         })}
     </div>
   )
